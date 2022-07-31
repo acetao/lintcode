249fc74579559ba2dd92c4b8bbff32f3e238e865
leety <acetao@163.com>
2022-07-31 21:15:56 +0800

    2022-07-31 21:13

---
+++ Main1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+    /**
+     * @param a: An integer
+     * @param b: An integer
+     * @return: The sum of a and b 
+     */
+    public int aplusb(int a, int b) {
+        return a + b;
+    }
+
+}
+public class Main1{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        Solution solution = new Solution();
+        int result = solution.aplusb(a, b);
+        System.out.println("a + b = " + result);
+
+        scanner.close();
+    }
+}
+
